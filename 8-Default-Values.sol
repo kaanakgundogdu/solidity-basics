f97eeb38d08a075be6e28d7e47e14a07f8459764
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-06-23 14:03:10 +0300

    Lecture 8 added

---
+++ 8-Default-Values.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+
+contract DefaultValues {
+
+    bool public defaultBool; // Default value of boolean is False    
+    uint public unsignedInt; // Default value -> 0
+    int public i; // Default value -> 0        
+    address public addr; // Default value -> 0x0000000000000000000000000000000000000000
+    bytes32 public b32; // Default value ->  0x0000000000000000000000000000000000000000000000000000000000000000
+}
